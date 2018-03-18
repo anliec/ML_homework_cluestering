3d8229b7e303ef66becac5a4c6b7535d2d638b77
Nicolas <nicolas.six@pau.fr>
2018-03-18 16:46:29 +0100

    fix data format issues

--- src/perceptron.py
+++ src/perceptron.py
@@ -6,6 +6,7 @@ import sys
 
 from keras.layers import Dense, Activation, BatchNormalization, InputLayer, Dropout
 from keras.models import Sequential
+from keras.utils import to_categorical
 
 from src.utils import *
 from src.reduce_dimensions import get_data
@@ -33,22 +34,24 @@ def get_model(feature_count, class_count, hidden_layer, use_batch_norm=True, opt
     return return_model
 
 
-def perceptron(number_of_feature, dim_reduction_algrithm, number_of_epoch, hidden_neurons=((35,),),
+def perceptron(number_of_features, dim_reduction_algrithm, number_of_epoch, hidden_neurons=((35,),),
                use_batch_norm_values=(True,), optimizer_values=('rmsprop',),
                activation_values=('sigmoid',), training_sizes=(-1,), out_file_name=None):
 
     df = pd.DataFrame()
-    for num_feature in number_of_feature:
+    for num_feature in number_of_features:
         for algo in dim_reduction_algrithm:
             x_learn, y_learn, x_test, y_test = get_data(num_feature, algo)
+            y_learn = to_categorical(y_learn, 200)
+            y_test = to_categorical(y_test, 200)
             for opt in optimizer_values:
                 for act in activation_values:
                     for use_batch_norm in use_batch_norm_values:
                         for layers in hidden_neurons:
                             for train_limit in training_sizes:
-                                number_of_feature = len(x_learn[0])
-                                number_of_class = len(y_learn[0])
-                                model = get_model(number_of_feature, number_of_class, layers, use_batch_norm, opt, act)
+                                _, features = x_learn.shape
+                                _, classes = y_learn.shape
+                                model = get_model(features, classes, layers, use_batch_norm, opt, act)
                                 h = model.fit(x=np.array(x_learn[:train_limit]),
                                               y=np.array(y_learn[:train_limit]),
                                               batch_size=len(x_learn[:train_limit]),
@@ -61,20 +64,20 @@ def perceptron(number_of_feature, dim_reduction_algrithm, number_of_epoch, hidde
                                 h_values = h.history.values()
                                 values = np.array([epoch, ] + list(h_values))
                                 tmp = pd.DataFrame(data=values.T, columns=["epoch", ] + list(h.history.keys()))
-                                tmp.assign(use_batch_norm=pd.Series([use_batch_norm] * number_of_epoch),
-                                           optimizer=pd.Series([opt] * number_of_epoch),
-                                           activation=pd.Series([act] * number_of_epoch),
-                                           layers=pd.Series([str(layers)] * number_of_epoch),
-                                           train_size=pd.Series([str(train_limit)] * number_of_epoch),
-                                           reduction_method=pd.Series([algo] * number_of_epoch),
-                                           number_of_feature=pd.Series([num_feature] * number_of_epoch))
+                                tmp = tmp.assign(use_batch_norm=pd.Series([use_batch_norm] * number_of_epoch))
+                                tmp = tmp.assign(optimizer=pd.Series([opt] * number_of_epoch))
+                                tmp = tmp.assign(activation=pd.Series([act] * number_of_epoch))
+                                tmp = tmp.assign(layers=pd.Series([str(layers)] * number_of_epoch))
+                                tmp = tmp.assign(train_size=pd.Series([str(train_limit)] * number_of_epoch))
+                                tmp = tmp.assign(reduction_method=pd.Series([algo] * number_of_epoch))
+                                tmp = tmp.assign(number_of_feature=pd.Series([num_feature] * number_of_epoch))
                                 if out_file_name is None:
-                                    df = df.append(tmp)
+                                    df = df.append(tmp, ignore_index=True)
                                 else:
                                     path = "stats/" + out_file_name + ".csv"
                                     if not os.path.exists("stats"):
                                         os.makedirs("stats")
-                                    if os.path.exists("stats/" + data_set_name + ".csv"):
+                                    if os.path.exists(path):
                                         tmp.to_csv(path_or_buf=path, mode='a', header=False)
                                     else:
                                         tmp.to_csv(path_or_buf=path)
@@ -90,9 +93,9 @@ if __name__ == "__main__":
     #     exit(1)
     # data_set_name = sys.argv[1]
     # if data_set_name == "starcraft":
-    result = perceptron(number_of_feature=range(5, 31, 5),
+    result = perceptron(number_of_features=range(5, 31, 5),
                         dim_reduction_algrithm=('PCA', 'ICA', 'random', 'LDA'),
-                        number_of_epoch=600,
+                        number_of_epoch=800,
                         hidden_neurons=((), (35,), (25,), (15,), (10,), (30, 10), (20, 10)),
                         use_batch_norm_values=(True,),
                         optimizer_values=('rmsprop',),
